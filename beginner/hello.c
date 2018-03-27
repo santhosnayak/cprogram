2f0a8fd22f51ea9ca819129feea0d1bc46c77d17
santhosnayak <37831124+santhosnayak@users.noreply.github.com>
2018-03-27 03:36:15 -0700

    Create hello.c

---
+++ beginner/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i;
+	printf("enter how many times to print Hello ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("\nHello");
+	}
+	// your code goes here
+	return 0;
+}
